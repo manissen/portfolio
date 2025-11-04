6e832d0a6f936614c6814694365b2941b501b3a7
manissen <manissen@ucsd.edu>
2025-11-03 22:52:56 -0800

    adding meta page

--- global.js
+++ global.js
@@ -45,6 +45,7 @@ const pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta'},
   { url: 'https://github.com/manissen/', title: 'GitHub' },
 ];
 
